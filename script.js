0d8ecafe43b0f13bd06d47111aad0b28504fbf09
Y8a0 <halidouyacouba999@gmail.com>
2023-10-10 14:41:59 +0100

    Fin du proget

--- script.js
+++ script.js
@@ -1,67 +1,68 @@
-const saiCategori = document.getElementById('saiCategori');
-const saiTitre = document.getElementById('saiTiter');
-const saiDate = document.getElementById('saiDate');
-const saiDescription = document.querySelector('.saiDescription');
-const saiStatu = document.getElementById('saiStatu');
-const listeAjout = document.querySelector('.listeAjout');
-const ajouter = document.getElementById('ajouter');
-const descruptionRecuper = document.getElementById('descruptionRecuper');
+const saiCategori = document.getElementById("saiCategori");
+const saiTitre = document.getElementById("saiTiter");
+const saiDate = document.getElementById("saiDate");
+const saiDescription = document.querySelector(".saiDescription");
+const saiStatu = document.getElementById("saiStatu");
+const listeAjout = document.querySelector(".listeAjout");
+const ajouter = document.getElementById("ajouter");
+const descruptionRecuper = document.getElementById("descruptionRecuper");
+const dan = document.querySelector(".essa");
 
 // Utilisez un tableau vide pour initialiser dataTache
 let dataTache = [];
 
 // Vérification de la présence des données dans le stockage local
 if (!localStorage.getItem("key")) {
-    localStorage.setItem("key", JSON.stringify(dataTache));
+  localStorage.setItem("key", JSON.stringify(dataTache));
 }
 
 // Récupérez le tableau depuis le stockage local
 dataTache = JSON.parse(localStorage.getItem("key"));
 
 function ajouterTache() {
-    const categorieValue = saiCategori.value;
-    const titreValue = saiTitre.value;
-    const dateValue = saiDate.value;
-    const descriptionValue = saiDescription.value;
-    const statutValue = saiStatu.value;
+  const categorieValue = saiCategori.value;
+  const titreValue = saiTitre.value;
+  const dateValue = saiDate.value;
+  const descriptionValue = saiDescription.value;
+  const statutValue = saiStatu.value;
 
-    // Ajoutez la nouvelle tâche au tableau dataTache
-    dataTache.push({
-        categorie: categorieValue,
-        titre: titreValue,
-        date: dateValue,
-        description: descriptionValue,
-        statut: statutValue
-    });
+  // Ajoutez la nouvelle tâche au tableau dataTache
+  dataTache.push({
+    categorie: categorieValue,
+    titre: titreValue,
+    date: dateValue,
+    description: descriptionValue,
+    statut: statutValue,
+  });
 
-    // Sauvegardez le tableau mis à jour dans le localStorage
-    localStorage.setItem("key", JSON.stringify(dataTache));
+  // Sauvegardez le tableau mis à jour dans le localStorage
+  localStorage.setItem("key", JSON.stringify(dataTache));
 
-    // Réinitialisez les champs de saisie
-    saiCategori.value = '';
-    saiTitre.value = '';
-    saiDate.value = '';
-    saiDescription.value = '';
-    saiStatu.value = '';
+  // Réinitialisez les champs de saisie
+  saiCategori.value = "";
+  saiTitre.value = "";
+  saiDate.value = "";
+  saiDescription.value = "";
+  saiStatu.value = "";
 
-    // Affichez les tâches
-    afficherTaches(dataTache);
+  // Affichez les tâches
+  afficherTaches(dataTache);
 }
 
 function afficherTaches(tache) {
-  descruptionRecuper.textContent = '';
-    listeAjout.innerHTML = ''; // Effacez le contenu actuel
-    tache.forEach((element, index) => {
-        const divAfficheTache = document.createElement('div');
-        divAfficheTache.classList.add('titleAjout');
-        divAfficheTache.innerHTML = `
+  descruptionRecuper.textContent = "";
+  listeAjout.innerHTML = ""; // Effacez le contenu actuel
+  tache.forEach((element, index) => {
+    const divAfficheTache = document.createElement("div");
+    divAfficheTache.classList.add("titleAjout");
+    divAfficheTache.innerHTML = `
         <span>${index + 1}</span>
         <span>${element.date}</span>
         <span>${element.titre}</span>
         <span>${element.categorie}</span>
         <span>
           <button
-            id="btnVusial"
+            id="btnVusial${index}"
             style="background-color: rgb(46, 107, 137)"
           >
             <i class="fa-solid fa-eye"></i>
@@ -74,49 +75,84 @@ function afficherTaches(tache) {
           </button>
         </span>
         `;
-        listeAjout.appendChild(divAfficheTache);
-        descruptionRecuper.textContent = element.description;
+    listeAjout.appendChild(divAfficheTache);
+    descruptionRecuper.textContent = element.description;
 
-// ------------------------------------------------------------
-const btnModifier = document.getElementById(`btnModifier${index}`);
-    btnModifier.addEventListener('click', () => {
-        // Récupérez la tâche à modifier en utilisant l'index
-        const tacheAModifier = dataTache[index];
-  
-        // Remplissez les champs de saisie avec les données actuelles de la tâche
-        saiCategori.value = tacheAModifier.categorie;
-        saiTitre.value = tacheAModifier.titre;
-        saiDate.value = tacheAModifier.date;
-        saiDescription.value = tacheAModifier.description;
-        saiStatu.value = tacheAModifier.statut;
-  
-        // Supprimez la tâche du tableau dataTache
-        dataTache.splice(index, 1);
-  
-        // Mettez à jour le localStorage avec le tableau modifié
-        localStorage.setItem("key", JSON.stringify(dataTache));
-  
-        // Affichez à nouveau la liste de tâches mise à jour
-        afficherTaches(dataTache);
-        affichgraph();
-        chart();
+    // ------------------------------------------------------------
+    const btnModifier = document.getElementById(`btnModifier${index}`);
+    btnModifier.addEventListener("click", () => {
+      // Récupérez la tâche à modifier en utilisant l'index
+      const tacheAModifier = dataTache[index];
+
+      // Remplissez les champs de saisie avec les données actuelles de la tâche
+      saiCategori.value = tacheAModifier.categorie;
+      saiTitre.value = tacheAModifier.titre;
+      saiDate.value = tacheAModifier.date;
+      saiDescription.value = tacheAModifier.description;
+      saiStatu.value = tacheAModifier.statut;
+
+      // Supprimez la tâche du tableau dataTache
+      dataTache.splice(index, 1);
+
+      // Mettez à jour le localStorage avec le tableau modifié
+      localStorage.setItem("key", JSON.stringify(dataTache));
+
+      // Affichez à nouveau la liste de tâches mise à jour
+      afficherTaches(dataTache);
+      affichgraph();
+      chart();
     });
-  
+
     const btnSupprimer = document.getElementById(`btnSupprimer${index}`);
-    btnSupprimer.addEventListener('click', () => {
-        // Supprimez la tâche du tableau dataTache en utilisant l'index
-        dataTache.splice(index, 1);
-  
-        // Mettez à jour le localStorage avec le tableau modifié
-        localStorage.setItem("key", JSON.stringify(dataTache));
-  
-        // Affichez à nouveau la liste de tâches mise à jour
-        afficherTaches(dataTache);
-        affichgraph();
-        chart();
+    btnSupprimer.addEventListener("click", () => {
+      // Supprimez la tâche du tableau dataTache en utilisant l'index
+      dataTache.splice(index, 1);
+
+      // Mettez à jour le localStorage avec le tableau modifié
+      localStorage.setItem("key", JSON.stringify(dataTache));
+
+      // Affichez à nouveau la liste de tâches mise à jour
+      afficherTaches(dataTache);
+      affichgraph();
+      chart();
     });
-//-------------------------------------------------------
+    //-------------------------------------------------------
+    const btnVusial = document.getElementById(`btnVusial${index}`);
+    console.log("====================================");
+    console.log(btnVusial);
+    console.log("====================================");
+    btnVusial.addEventListener("click", (e) => {
+      if (e.target) {
+        esaai.style.display = "block";
+        let fermer = document.querySelector(".fermer");
+        fermer.style.display = "block";
+      }
     });
+    /* */
+    /* */
+    const esaai = document.createElement("div");
+    esaai.classList.add("divVisible");
+    esaai.innerHTML = `
+        <h3 class="pm"> informations tâche</h3>
+        <div class="contenu">
+          <div class="contenuTitre">
+            <span >Date </span>
+            <span >Titre </span>
+            <span >Catégorie </span>
+            <span >Description </span>
+            <span >Statut </span>
+          </div>
+          <div class="contenuLibele">
+          <span>${element.date}</span>
+          <span>${element.titre}</span>
+          <span>${element.categorie}</span>
+            <span> ${element.description} </span>
+            <span> ${element.statut} </span>
+          </div>
+        </div>
+    `;
+    dan.appendChild(esaai);
+  });
 }
 
 // Écoutez le clic sur le bouton "ajouter" et appelez la fonction ajouterTache
@@ -131,67 +167,65 @@ ajouter.addEventListener("click", (event) => {
 // Au chargement de la page, affichez les tâches depuis le localStorage
 afficherTaches(dataTache);
 
-
 let terminer = 0;
 let niveau = 0;
 let encour = 0;
 
-affichgraph()
- let myChart;
+affichgraph();
+let myChart;
 function chart() {
-const ctx = document.getElementById('myChart');
-if(myChart){
-    myChart.destroy()
-}
- myChart = new Chart(ctx, {
-    type: 'pie',
+  const ctx = document.getElementById("myChart");
+  if (myChart) {
+    myChart.destroy();
+  }
+  myChart = new Chart(ctx, {
+    type: "pie",
     data: {
-        labels: ['terminer', 'nouveau', 'encours'],
-      datasets: [{
-        data: [terminer, nouveau, encour],
-        backgroundColor:['gray', 'blue', 'green'],
-        borderWidth: 1
-      }]
+      labels: ["terminer", "nouveau", "encours"],
+      datasets: [
+        {
+          data: [terminer, nouveau, encour],
+          backgroundColor: ["gray", "blue", "green"],
+          borderWidth: 1,
+        },
+      ],
     },
-    
   });
-  
 }
-chart()
+chart();
 
 function affichgraph() {
-    terminer = 0
-    nouveau = 0
-    encour = 0
-    dataTache.forEach(element => {
-        if (element.statut==="Terminé") {
-            terminer++ ;
-        }else  if (element.statut==="Nouveau") {
-            nouveau++;
-        } else  if (element.statut==="En cours") {
-            encour ++;
-        }
-    });
-} 
+  terminer = 0;
+  nouveau = 0;
+  encour = 0;
+  dataTache.forEach((element) => {
+    if (element.statut === "Terminé") {
+      terminer++;
+    } else if (element.statut === "Nouveau") {
+      nouveau++;
+    } else if (element.statut === "En cours") {
+      encour++;
+    }
+  });
+}
 
-
-const notification = document.querySelector('.notification');
-const textModifie = document.getElementById('textModifie');
-textModifie.textContent = '';
-function message(){
-  if (saiCategori.value === "" ||
-  saiTitre.value === "" ||
-  saiDate.value === "" ||
-  saiDescription.value === "" ||
-  saiStatu.value === "" 
+const notification = document.querySelector(".notification");
+const textModifie = document.getElementById("textModifie");
+textModifie.textContent = "";
+function message() {
+  if (
+    saiCategori.value === "" ||
+    saiTitre.value === "" ||
+    saiDate.value === "" ||
+    saiDescription.value === "" ||
+    saiStatu.value === ""
   ) {
     notification.classList.remove("notification");
     notification.classList.add("notificationn");
-    textModifie.textContent = 'Erreur! Veuillez remplir les champs';
+    textModifie.textContent = "Erreur! Veuillez remplir les champs";
     setTimeout(() => {
       notification.classList.add("notification");
-    notification.classList.remove("notificationn");
+      notification.classList.remove("notificationn");
     }, 2000);
   }
 }
-
